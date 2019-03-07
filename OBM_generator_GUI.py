7a8db415131b336dbc19a92a2cfb2df45cb32406
Brett Behler <bgbehler@gmail.com>
2019-03-07 11:25:57 -0500

    PEP8 formatting compliance.

--- OBM_generator_GUI.py
+++ OBM_generator_GUI.py
@@ -1,14 +1,14 @@
 #! python3
-## OBM_generator_GUI.py
-## tkinter based GUI for creating an OBM file for OpenTTD
-## Brett Behler 03.05.2019
+# OBM_generator_GUI.py
+# tkinter based GUI for creating an OBM file for OpenTTD
+# Brett Behler 03.05.2019
 
 import tkinter as tk
 from tkinter import filedialog
 import openttd_obm_generator as obm
 import os
 
-## Global Variables
+# Global Variables
 
 LARGE_FONT = ('fixedsys', 12)
 MEDIUM_FONT = ('fixedsys', 10)
@@ -17,81 +17,90 @@ GREY = '#838582'
 BLACK = '#101010'
 BLUE = '#a0c2de'
 
-## Event Commands
+# Event Commands
+
 
 def add_track(track, style):
     pass
-    #add track to style list
+    # add track to style list
+
 
 def remove_track(track, style):
     pass
-    #remove track from style list
+    # remove track from style list
+
 
 def clear_style(style):
     pass
 
+
 def popupmessage(message):
     pass
-    #display popup window with relevant error information
+    # display popup window with relevant error information
+
 
 def new_obm():
     pass
-    #if OBMApp.obm_data, popup overwrite warning
-    #else, OMBApp.set_obm = obm.OBMData()
+    # f OBMApp.obm_data, popup overwrite warning
+    # else, OMBApp.set_obm = obm.OBMData()
+
 
 def open_obm():
     pass
-    #tkinter filedialog
-    #read data
-    #OBMApp.set_obm(name, shortname, version, description)
-    #OBMAApp.obm_data.write_lists(theme, old, new, ez)
+    # tkinter filedialog
+    # read data
+    # OBMApp.set_obm(name, shortname, version, description)
+    # OBMAApp.obm_data.write_lists(theme, old, new, ez)
+
 
 def save_obm():
     pass
-    #validate_data()
-    #tkinter filedialog
-    #if True, save obm file in directory
-    #else, popupmessage
+    # validate_data()
+    # tkinter filedialog
+    # if True, save obm file in directory
+    # else, popupmessage
+
 
 def validate_data(OBMData):
     pass
-    #if valid, True
-    #else, False
+    # if valid, True
+    # else, False
+
 
 def quit_program():
     if not app.has_saved:
         pass
-        #popupmessage changes lost warning
+        # popupmessage changes lost warning
     else:
         raise SystemExit
 
-## Main App
+# Main App
 
 
 class OBMApp(tk.Tk):
-    '''Main tkinter app.
-
+    """Does these things.
+    
+    Instance Variables:
+        
+    
     Methods:
-        __init__(self):
-        create_widgets(self):
-        set_obm(self, OBMData):
-        import_tracks():
-
-    '''
+        
+    
+    """
     def __init__(self):
         """Doc string text
-        
+
         Arguments:
-            
-        
+
+
         Keyword Arguments:
-            
-        
+
+
         Returns:
-            
-        
+
+
         Raises:
-            
+
         """
         global GREY
 
@@ -111,12 +120,11 @@ class OBMApp(tk.Tk):
         self.cur_track = None
 
         self.container = tk.Frame(self, bg=GREY)
-        # self.container.grid(row=0, column=0, sticky='NSEW')
 
         self.create_widgets()
 
     def create_widgets(self):
-        """Creates and configures all of the GUI widgets."""
+        """Create and configure all GUI widgets."""
 
         global LARGE_FONT
         global MEDIUM_FONT
@@ -125,7 +133,7 @@ class OBMApp(tk.Tk):
         global BLACK
         global BLUE
 
-        ## Menubar Configuration
+        # Menubar Configuration
         menubar = tk.Menu(self.container)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label='New', command=new_obm)
@@ -140,99 +148,153 @@ class OBMApp(tk.Tk):
 
         tk.Tk.config(self, menu=menubar)
 
-        ## Metadata Configuration
+        # Metadata Configuration
 
-        tk.Label(self, text='OBM Metadata', font=LARGE_FONT, bg=GREY).grid(row=0, column=0, columnspan=5, sticky='NW', padx=5, pady=10)
+        tk.Label(self, text='OBM Metadata', font=LARGE_FONT, bg=GREY).grid(
+            row=0, column=0, columnspan=5, sticky='NW', padx=5, pady=10)
 
         self.meta_name_val = tk.StringVar()
-        tk.Label(self, text='Name:', font=MEDIUM_FONT, bg=GREY).grid(row=1, column=0, sticky='NW', padx=5, pady=1)
-        self.meta_name = tk.Entry(self, textvariable=self.meta_name_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.meta_name.grid(row=1, column=1, columnspan=4, sticky='NEW', padx=5)
+        tk.Label(self, text='Name:', font=MEDIUM_FONT, bg=GREY).grid(row=1,
+                column=0, sticky='NW', padx=5, pady=1)
+        self.meta_name = tk.Entry(self, textvariable=self.meta_name_val)
+        self.meta_name.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.meta_name.grid(row=1, column=1, columnspan=4, sticky='NEW',
+                padx=5)
         self.meta_name.insert(0, 'Music Pack Name')
 
         self.shortname_val = tk.StringVar()
         self.shortname_val.trace('w', self.limit_shortname)
-        tk.Label(self, text='Shortname:', font=MEDIUM_FONT, bg=GREY).grid(row=2, column=0, sticky='NW', padx=5, pady=1)
-        self.meta_shortname = tk.Entry(self, textvariable=self.shortname_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.meta_shortname.grid(row=2, column=1, columnspan=4, sticky='NEW', padx=5)
+        tk.Label(self, text='Shortname:', font=MEDIUM_FONT, bg=GREY).grid(
+            row=2, column=0, sticky='NW', padx=5, pady=1)
+        self.meta_shortname = tk.Entry(self, textvariable=self.shortname_val)
+        self.meta_shortname.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.meta_shortname.grid(row=2, column=1, columnspan=4, sticky='NEW',
+                padx=5)
         self.meta_shortname.insert(0, 'XXXX')
 
         self.version_val = tk.StringVar()
-        tk.Label(self, text='Version:', font=MEDIUM_FONT, bg=GREY).grid(row=3, column=0, sticky='NW', padx=5, pady=1)
-        self.meta_version = tk.Entry(self, textvariable=self.version_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.meta_version.grid(row=3, column=1, columnspan=4, sticky='NEW', padx=5)
+        tk.Label(self, text='Version:', font=MEDIUM_FONT, bg=GREY).grid(row=3,
+                column=0, sticky='NW', padx=5, pady=1)
+        self.meta_version = tk.Entry(self, textvariable=self.version_val)
+        self.meta_version.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.meta_version.grid(row=3, column=1, columnspan=4, sticky='NEW',
+                padx=5)
         self.meta_version.insert(0, '0.0.1')
 
         self.description_val = tk.StringVar()
-        tk.Label(self, text='Description:', font=MEDIUM_FONT, bg=GREY).grid(row=4, column=0, sticky='NW', padx=5, pady=1)
-        self.meta_description = tk.Entry(self, textvariable=self.description_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.meta_description.grid(row=4, column=1, columnspan=4, sticky='NEW', padx=5)
-        self.meta_description.insert(0, 'A short description for the music pack')
+        tk.Label(self, text='Description:', font=MEDIUM_FONT, bg=GREY).grid(
+                row=4, column=0, sticky='NW', padx=5, pady=1)
+        self.meta_description = tk.Entry(self,
+                textvariable=self.description_val)
+        self.meta_description.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.meta_description.grid(row=4, column=1, columnspan=4, sticky='NEW',
+                padx=5)
+        self.meta_description.insert(0, ('A short description'
+                                        + 'for the music pack'))
 
         self.origin_val = tk.StringVar()
-        tk.Label(self, text='Origin:', font=MEDIUM_FONT, bg=GREY).grid(row=5, column=0, sticky='NW', padx=5, pady=1)
-        self.meta_origin = tk.Entry(self, textvariable=self.origin_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.meta_origin.grid(row=5, column=1, columnspan=4, sticky='NEW', padx=5)
+        tk.Label(self, text='Origin:', font=MEDIUM_FONT, bg=GREY).grid(row=5,
+                column=0, sticky='NW', padx=5, pady=1)
+        self.meta_origin = tk.Entry(self, textvariable=self.origin_val)
+        self.meta_origin.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.meta_origin.grid(row=5, column=1, columnspan=4, sticky='NEW',
+                padx=5)
         self.meta_origin.insert(0, 'https://')
 
-
-        ## Midifile Configuration
-        tk.Label(self, text='Track Information: ', font=LARGE_FONT, bg=GREY).grid(row=6, column=0, sticky='NW', padx=5, pady=10)
+        # Midifile Configuration
+        tk.Label(self, text='Track Information: ', font=LARGE_FONT,
+                bg=GREY).grid(row=6, column=0, sticky='NW', padx=5, pady=10)
         self.track_label = tk.Label(self, font=LARGE_FONT, bg=GREY)
-        self.track_label.grid(row=6, column=1, columnspan=3, sticky='NEW', pady=10, padx=5)
+        self.track_label.grid(row=6, column=1, columnspan=3, sticky='NEW',
+                pady=10, padx=5)
 
         self.track_name_val = tk.StringVar()
-        tk.Label(self, text='Name:', font=MEDIUM_FONT, bg=GREY).grid(row=7, column=0, padx=5, pady=1, sticky='NW')
-        self.track_name = tk.Entry(self, textvariable=self.track_name_val, bg=BLACK, fg=BLUE, font=MEDIUM_FONT)
-        self.track_name.grid(row=7, column=1, columnspan=4, sticky='NEW', padx=5)
+        tk.Label(self, text='Name:', font=MEDIUM_FONT, bg=GREY).grid(row=7,
+                column=0, padx=5, pady=1, sticky='NW')
+        self.track_name = tk.Entry(self, textvariable=self.track_name_val)
+        self.track_name.config(bg=BLACK, fg=BLUE, font=MEDIUM_FONT,
+                insertbackground='white')
+        self.track_name.grid(row=7, column=1, columnspan=4, sticky='NEW',
+                padx=5)
         self.track_name.insert(0, 'Name of current track')
 
-        ## Style Configuration
-        tk.Label(self, text='TRACK INDEX', font=LARGE_FONT, bg=GREY).grid(row=8, column=0, sticky='NSE', padx=5, pady=10)
-        import_btn = tk.Button(self, text='IMPORT...', command=self.import_tracks, width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        # Style Configuration
+        tk.Label(self, text='TRACK INDEX', font=LARGE_FONT, bg=GREY).grid(
+            row=8, column=0, sticky='NSE', padx=5, pady=10)
+        import_btn = tk.Button(self, text='IMPORT...',
+                command=self.import_tracks, width=15, font=SMALL_FONT, bg=GREY,
+                fg=BLACK)
         import_btn.grid(row=8, column=1, sticky='NE', pady=10, padx=5)
 
-        tk.Label(self, text='PROGRAM -', font=LARGE_FONT, bg=GREY).grid(row=8, column=3, sticky='NSE', padx=5, pady=10)
+        tk.Label(self, text='PROGRAM -', font=LARGE_FONT, bg=GREY).grid(row=8,
+            column=3, sticky='NSE', padx=5, pady=10)
         self.style_label = tk.Label(self, font=LARGE_FONT, bg=GREY)
         self.style_label.grid(row=8, column=4, sticky='NSW', padx=5, pady=10)
 
-        self.track_listbox = tk.Listbox(self, bg=BLACK, fg=BLUE, width=40, font=SMALL_FONT)
-        self.track_listbox.grid(row=9, column=0, columnspan=2, rowspan=8, sticky='NSEW', padx=5)
+        self.track_listbox = tk.Listbox(self, bg=BLACK, fg=BLUE, width=40,
+                font=SMALL_FONT)
+        self.track_listbox.grid(row=9, column=0, columnspan=2, rowspan=8,
+                sticky='NSEW', padx=5)
 
-        self.style_listbox = tk.Listbox(self, bg=BLACK, fg=BLUE, width=40, font=SMALL_FONT)
-        self.style_listbox.grid(row=9, column=3, columnspan=2, rowspan=8, sticky='NSEW', padx=5)
+        self.style_listbox = tk.Listbox(self, bg=BLACK, fg=BLUE, width=40,
+                font=SMALL_FONT)
+        self.style_listbox.grid(row=9, column=3, columnspan=2, rowspan=8,
+                sticky='NSEW', padx=5)
 
-        self.theme_btn = tk.Button(self, text='THEME', command=lambda: self.switch_style('theme'), width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.theme_btn = tk.Button(self, text='THEME',
+                command=lambda: self.switch_style('theme'), width=15,
+                font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.theme_btn.grid(row=9, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.old_btn = tk.Button(self, text='OLD STYLE', command=lambda: self.switch_style('old'), width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.old_btn = tk.Button(self, text='OLD STYLE',
+                command=lambda: self.switch_style('old'), width=15,
+                font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.old_btn.grid(row=10, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.new_btn = tk.Button(self, text='NEW STYLE', command=lambda: self.switch_style('new'), width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.new_btn = tk.Button(self, text='NEW STYLE',
+                command=lambda: self.switch_style('new'), width=15,
+                font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.new_btn.grid(row=11, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.ezy_btn = tk.Button(self, text='EZY STREET', command=lambda: self.switch_style('ezy'), width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.ezy_btn = tk.Button(self, text='EZY STREET',
+                command=lambda: self.switch_style('ezy'), width=15,
+                font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.ezy_btn.grid(row=12, column=2, sticky='NSEW', padx=10, pady=10)
 
         tk.Label(self, text='', bg=GREY).grid(row=13, column=2, sticky='NSEW')
 
-        self.add_btn = tk.Button(self, text='ADD >>', command=add_track, width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.add_btn = tk.Button(self, text='ADD >>', command=add_track,
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.add_btn.grid(row=14, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.ezy_btn = tk.Button(self, text='<< REMOVE', command=remove_track, width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.ezy_btn = tk.Button(self, text='<< REMOVE', command=remove_track,
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.ezy_btn.grid(row=15, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.ezy_btn = tk.Button(self, text='CLEAR', command=clear_style, width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.ezy_btn = tk.Button(self, text='CLEAR', command=clear_style,
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
         self.ezy_btn.grid(row=16, column=2, sticky='NSEW', padx=10, pady=10)
 
+        self.save_btn = tk.Button(self, text='SAVE OBM', command=save_obm,
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.save_btn.grid(row=17, column=4, sticky='SE', padx=5, pady=10)
+
     def limit_shortname(self, *args):
+        """Set shortname_val to four characters. Capitalize letters."""
         text = self.meta_shortname.get()[:4]
         self.shortname_val.set(text.upper())
-    
+
     def set_obm(self, *args, **kwargs):
+        """Assign self.obm_data to new OBMData object."""
         self.obm_data = obm.OBMData(*args, **kwargs)
 
     def switch_style(self, style):
+        """Assign new self.style_list values based on chosen style."""
         self.cur_style = style
         self.style_list = []
         if style == 'old':
@@ -250,6 +312,7 @@ class OBMApp(tk.Tk):
         self.update()
 
     def import_tracks(self):
+        """Append compatible files in chosen directory to self.track_list."""
         directory = filedialog.askdirectory()
         for f in os.listdir(directory):
             if (os.path.isfile(os.path.join(directory, f))
@@ -260,6 +323,7 @@ class OBMApp(tk.Tk):
         self.update()
 
     def update(self):
+        """Update self.track_listbox and self.style_listbox to new values."""
         self.track_listbox.delete(0, tk.END)
         for track in self.track_list:
             self.track_listbox.insert(tk.END, track)
@@ -267,7 +331,6 @@ class OBMApp(tk.Tk):
         for track in self.style_list:
             self.style_listbox.insert(tk.END, track)
 
-    
 
 if __name__ == '__main__':
     app = OBMApp()
--- openttd_obm_generator.py
+++ openttd_obm_generator.py
@@ -1,39 +1,91 @@
-# ! python3
-## openttd_obm_generator.py
-## Generates a .obm file for use by OpenTTD from a directory
-## Brett Behler 12.20.2018
+#! python3
+# openttd_obm_generator.py
+# Generates a .obm file for use by OpenTTD from a directory
+# Brett Behler 12.20.2018
 
 import hashlib
 import os
 
+
 class OBMData(object):
-    def __init__(self, 
-        name='New OBM', 
-        shortname='XXXX', 
-        version='*', 
-        description='*', 
-        origin='*'
-    ):
+    """Does these things.
+    
+    Instance Variables:
+        
+
+    Methods:
+        
+    """
+    def __init__(self,
+            name='New OBM',
+            shortname='XXXX',
+            version='*',
+            description='*',
+            origin='*'):
+        """Instantiate and return OBMData object.
+
+        Keyword Arguments:
+            name:(str) -- The name for metadata.
+            shortname:(str) -- The shortname for metadata.
+            version:(str) -- The music pack version number.
+            description:(str) -- Short description for music pack.
+            origin:(str) -- URL for music pack download.
+
+        Returns:
+            OBMData object.
+
+        Raises:
+            None
+        """
+
         # self.read_directory()
         self.meta_data = {
-            'name':name, 
-            'shortname':shortname,
-            'version':version,
-            'description':description,
-            'origin':origin
-        }
+            'name': name,
+            'shortname': shortname,
+            'version': version,
+            'description': description,
+            'origin': origin}
+
         self.theme = []
         self.old_style = []
         self.new_style = []
         self.ezy_street = []
 
     def write_lists(self, theme, old, new, ezy):
+        """Do this. Return that.
+        
+        Arguments:
+            
+        
+        Keyword Arguments:
+            
+        
+        Returns:
+            
+        
+        Raises:
+            
+        """
         self.theme = theme
         self.old_style = old
         self.new_style = new
         self.ezy_street = ezy
 
     def md5(self, fname):
+        """Do this. Return that.
+        
+        Arguments:
+            
+        
+        Keyword Arguments:
+            
+        
+        Returns:
+            
+        
+        Raises:
+            
+        """
         hash_md5 = hashlib.md5()
         with open(fname, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
@@ -41,6 +93,20 @@ class OBMData(object):
         return hash_md5.hexdigest()
 
     def read_directory(self):
+        """Do this. Return that.
+        
+        Arguments:
+            
+        
+        Keyword Arguments:
+            
+        
+        Returns:
+            
+        
+        Raises:
+            
+        """
         # get list of valid midi files in current directory
         self.name = os.path.split(os.getcwd())[-1]
         i = 0
@@ -54,43 +120,58 @@ class OBMData(object):
                 if i == 0:
                     prefix = 'theme'
                 elif i < 11:
-                    prefix = 'old_' + str(i-1)
+                    prefix = 'old_' + str(i - 1)
                 elif i < 21:
-                    prefix = 'new_' + str((i-1) % 10)
+                    prefix = 'new_' + str((i - 1) % 10)
                 else:
-                    prefix = 'ezy_' + str((i-1) % 10)
+                    prefix = 'ezy_' + str((i - 1) % 10)
                 prefix += ' = '
                 self.obm_files_text.append(prefix + f)
                 self.obm_md5s_text.append(f + ' = ' + self.md5(f))
-                # file naming convention XX_track_title_name.mid where XX is the numerical track number
-                self.obm_names_text.append(f + ' = ' + ' '.join(f[3:-len(extension)].split('_')).title())
+                # file naming convention XX_track_title_name.mid where XX is
+                # the numerical track number
+                self.obm_names_text.append(f + ' = ' + ' '.join(
+                    f[3:-len(extension)].split('_')).title())
                 i += 1
-        # if the [files] section is shorter than the required 31 tracks, finish the [files] section.
+        # if the [files] section is shorter than the required 31 tracks, finish
+        # the [files] section.
         if len(self.obm_files_text) - 1 < 31:
             start = len(self.obm_files_text) - 1
             for i in range(start, 31):
                 if i == 0:
                     prefix = 'theme'
                 elif i < 11:
-                    prefix = 'old_' + str(i-1)
+                    prefix = 'old_' + str(i - 1)
                 elif i < 21:
-                    prefix = 'new_' + str((i-1) % 10)
+                    prefix = 'new_' + str((i - 1) % 10)
                 else:
-                    prefix = 'ezy_' + str((i-1) % 10)
+                    prefix = 'ezy_' + str((i - 1) % 10)
                 prefix += ' = '
                 self.obm_files_text.append(prefix)
 
-
     def create_file(self):
-        with open(self.name+'.obm', 'w') as obm_file:
+        """Do this. Return that.
+        
+        Arguments:
+            
+        
+        Keyword Arguments:
+            
+        
+        Returns:
+            
+        
+        Raises:
+            
+        """
+        with open(self.name + '.obm', 'w') as obm_file:
             text = [
-                '[metadata]', 
+                '[metadata]',
                 'name = ' + self.name,
-                'shortname = '+ self.name[:4].upper(),
+                'shortname = ' + self.name[:4].upper(),
                 'version = *',
                 'description = *',
-                '\n'
-                ]
+                '\n']
             obm_file.write('\n'.join(text))
             obm_file.write('\n'.join(self.obm_files_text))
             obm_file.write('\n\n')
@@ -101,5 +182,4 @@ class OBMData(object):
             obm_file.write('\n'.join(['[origin]', 'default = *']))
 
 if __name__ == '__main__':
-    gen_obm = OBMData()
-    gen_obm.create_file()
+    print('A class for tracking and formatting data into the OBM format.')
