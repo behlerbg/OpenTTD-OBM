157921b3bd025c940451172fc28a9366bd15c9e6
Brett Behler <bgbehler@gmail.com>
2019-03-08 20:01:38 -0500

    Mostly functional GUI.

--- OBM_generator_GUI.py
+++ OBM_generator_GUI.py
@@ -14,6 +14,8 @@ LARGE_FONT = ('fixedsys', 14)
 MEDIUM_FONT = ('fixedsys', 10)
 SMALL_FONT = ('fixedsys', 6)
 GREY = '#838582'
+DRK_GREY = '#50524f'
+LIGHT_GRAY = '#a8a8a8'
 BLACK = '#101010'
 BLUE = '#a0c2de'
 RED = '#e60005'
@@ -23,7 +25,7 @@ LIGHT_YELLOW = '#faf36f'
 # Event Commands
 
 
-def popupmessage(message):
+def popupinfo(message):
     popup = tk.Tk()
     popup.configure(bg=RED)
     popup.wm_title('Information')
@@ -36,25 +38,11 @@ def popupmessage(message):
 
 
 def new_obm():
-    pass
-    # f OBMApp.obm_data, popup overwrite warning
-    # else, OMBApp.set_obm = obm.OBMData()
+    popupinfo('Not yet implemented.')
 
 
 def open_obm():
-    pass
-    # tkinter filedialog
-    # read data
-    # OBMApp.set_obm(name, shortname, version, description)
-    # OBMAApp.obm_data.write_lists(theme, old, new, ez)
-
-
-def save_obm():
-    pass
-    # validate_data()
-    # tkinter filedialog
-    # if True, save obm file in directory
-    # else, popupmessage
+    popupinfo('Not yet implemented.')
 
 
 def validate_data(OBMData):
@@ -113,7 +101,7 @@ class OBMApp(tk.Tk):
         self.set_obm()
         self.has_saved = False
         self.cur_style = None
-        self.cur_track = None
+        self.confirm = None
 
         self.container = tk.Frame(self, bg=GREY)
 
@@ -134,13 +122,14 @@ class OBMApp(tk.Tk):
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label='New', command=new_obm)
         filemenu.add_command(label='Open...', command=open_obm)
-        filemenu.add_command(label='Save', command=save_obm)
-        filemenu.add_command(label='Save As...', command=save_obm)
+        filemenu.add_command(label='Save',
+                command=lambda: self.save_obm(self.openttd_dir))
+        filemenu.add_command(label='Save As...', command=self.save_obm)
         filemenu.add_separator()
         filemenu.add_command(label='Exit', command=quit_program)
         menubar.add_cascade(label='File', menu=filemenu)
 
-        menubar.add_command(label='Help', command=lambda: popupmessage('test'))
+        menubar.add_command(label='Help', command=lambda: popupinfo('test'))
 
         tk.Tk.config(self, menu=menubar)
 
@@ -206,7 +195,7 @@ class OBMApp(tk.Tk):
         tk.Label(self, text='Track Information: ', font=LARGE_FONT,
                 bg=GREY).grid(row=6, column=0, sticky='NW', padx=5, pady=10)
         self.track_label = tk.Label(self, font=LARGE_FONT, bg=GREY)
-        self.track_label.grid(row=6, column=1, columnspan=3, sticky='NEW',
+        self.track_label.grid(row=6, column=1, columnspan=3, sticky='NW',
                 pady=10, padx=5)
 
         self.track_name_val = tk.StringVar()
@@ -222,10 +211,11 @@ class OBMApp(tk.Tk):
         # Style Configuration
         tk.Label(self, text='TRACK INDEX', font=LARGE_FONT, bg=GREY).grid(
             row=8, column=0, sticky='NSE', padx=5, pady=10)
-        import_btn = tk.Button(self, text='IMPORT...',
+        self.import_btn = tk.Button(self, text='IMPORT...',
                 command=self.import_tracks, width=15, font=SMALL_FONT, bg=GREY,
-                fg=BLACK)
-        import_btn.grid(row=8, column=1, sticky='NE', pady=10, padx=5)
+                fg=BLACK, disabledforeground=BLACK,
+                activebackground=LIGHT_GRAY)
+        self.import_btn.grid(row=8, column=1, sticky='NE', pady=10, padx=5)
 
         tk.Label(self, text='PROGRAM -', font=LARGE_FONT, bg=GREY).grid(row=8,
             column=3, sticky='NSE', padx=5, pady=10)
@@ -250,40 +240,48 @@ class OBMApp(tk.Tk):
 
         self.theme_btn = tk.Button(self, text='THEME',
                 command=lambda: self.switch_style('theme'), width=15,
-                font=SMALL_FONT, bg=GREY, fg=BLACK)
+                font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.theme_btn.grid(row=9, column=2, sticky='NSEW', padx=10, pady=10)
 
         self.old_btn = tk.Button(self, text='OLD STYLE',
                 command=lambda: self.switch_style('old'), width=15,
-                font=SMALL_FONT, bg=GREY, fg=BLACK)
+                font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.old_btn.grid(row=10, column=2, sticky='NSEW', padx=10, pady=10)
 
         self.new_btn = tk.Button(self, text='NEW STYLE',
                 command=lambda: self.switch_style('new'), width=15,
-                font=SMALL_FONT, bg=GREY, fg=BLACK)
+                font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.new_btn.grid(row=11, column=2, sticky='NSEW', padx=10, pady=10)
 
         self.ezy_btn = tk.Button(self, text='EZY STREET',
                 command=lambda: self.switch_style('ezy'), width=15,
-                font=SMALL_FONT, bg=GREY, fg=BLACK)
+                font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.ezy_btn.grid(row=12, column=2, sticky='NSEW', padx=10, pady=10)
 
         tk.Label(self, text='', bg=GREY).grid(row=13, column=2, sticky='NSEW')
 
         self.add_btn = tk.Button(self, text='ADD >>', command=self.add_track,
-                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.add_btn.grid(row=14, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.ezy_btn = tk.Button(self, text='<< REMOVE', command=self.remove_track,
-                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
-        self.ezy_btn.grid(row=15, column=2, sticky='NSEW', padx=10, pady=10)
+        self.remove_btn = tk.Button(self, text='<< REMOVE',
+                command=self.remove_track, width=15, font=SMALL_FONT, bg=GREY,
+                fg=BLACK, activebackground=LIGHT_GRAY)
+        self.remove_btn.grid(row=15, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.ezy_btn = tk.Button(self, text='CLEAR', command=self.clear_style,
-                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
-        self.ezy_btn.grid(row=16, column=2, sticky='NSEW', padx=10, pady=10)
+        self.clear_btn = tk.Button(self, text='CLEAR',
+                command=self.clear_style, width=15, font=SMALL_FONT, bg=GREY,
+                fg=BLACK, activebackground=LIGHT_GRAY)
+        self.clear_btn.grid(row=16, column=2, sticky='NSEW', padx=10, pady=10)
 
-        self.save_btn = tk.Button(self, text='SAVE OBM', command=save_obm,
-                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK)
+        self.save_btn = tk.Button(self, text='SAVE OBM', command=self.save_obm,
+                width=15, font=SMALL_FONT, bg=GREY, fg=BLACK,
+                activebackground=LIGHT_GRAY)
         self.save_btn.grid(row=17, column=4, sticky='SE', padx=5, pady=10)
 
     def track_text(self, *args):
@@ -303,33 +301,71 @@ class OBMApp(tk.Tk):
         """Assign self.obm_data to new OBMData object."""
         self.obm_data = obm.OBMData(*args, **kwargs)
 
+    def get_confirm(self, popup, answer):
+        popup.destroy()
+        if answer == 'yes':
+            self.confirm = True
+        else:
+            self.confirm = False
+
+    def popup_yes_no(self, question):
+        self.confirm = None
+        popup = tk.Tk()
+        popup.configure(bg=RED)
+        popup.wm_title('Information')
+        label = tk.Label(popup, text=question, font=MEDIUM_FONT)
+        label.configure(bg=RED, fg=LIGHT_YELLOW)
+        label.pack(side='top', fill='x', pady=20, padx=30)
+        y_btn = tk.Button(popup, text='YES',
+                command=lambda: self.get_confirm(popup, 'yes'))
+        y_btn.configure(bg=YELLOW, fg=BLACK, font=SMALL_FONT, width=10)
+        y_btn.pack(side='right', padx=10, pady=10)
+        n_btn = tk.Button(popup, text='NO',
+                command=lambda: self.get_confirm(popup, 'no'))
+        n_btn.configure(bg=YELLOW, fg=BLACK, font=SMALL_FONT, width=10)
+        n_btn.pack(side='left', padx=10, pady=10)
+
     def switch_style(self, style):
         """Assign new self.style_list values based on chosen style."""
         self.cur_style = style
         self.style_list = []
+        buttons = [self.theme_btn, self.old_btn, self.new_btn, self.ezy_btn]
+        for button in buttons:
+            button.config(relief=tk.RAISED, bg=GREY)
         if style == 'old':
+            self.old_btn.config(relief=tk.SUNKEN, bg=LIGHT_GRAY)
             self.style_list = self.obm_data.old_style
             self.style_label['text'] = '"OLD STYLE"'
         elif style == 'new':
+            self.new_btn.config(relief=tk.SUNKEN, bg=LIGHT_GRAY)
             self.style_list = self.obm_data.new_style
             self.style_label['text'] = '"NEW STYLE"'
         elif style == 'ezy':
+            self.ezy_btn.config(relief=tk.SUNKEN, bg=LIGHT_GRAY)
             self.style_list = self.obm_data.ezy_street
             self.style_label['text'] = '"EZY STREET"'
         elif style == 'theme':
+            self.theme_btn.config(relief=tk.SUNKEN, bg=LIGHT_GRAY)
             self.style_list = self.obm_data.theme
             self.style_label['text'] = '"THEME"'
         self.update()
 
     def import_tracks(self):
         """Append compatible files in chosen directory to self.track_list."""
-        directory = filedialog.askdirectory()
-        for f in os.listdir(directory):
-            if (os.path.isfile(os.path.join(directory, f))
+        # if self.track_list:
+        #     self.popup_yes_no("""This will erase current track list.
+        #         Do you wish to continue?""")
+        #     if self.confirm == False:
+        #         return
+        #     self.track_list = []
+        self.music_dir = filedialog.askdirectory()
+        for f in os.listdir(self.music_dir):
+            if (os.path.isfile(os.path.join(self.music_dir, f))
                     and f[-3:] == '.gm'
                     or f[-4:] == '.mid'
                     or f[-5:] == '.midi'):
                 self.track_list.append(f)
+        self.import_btn.config(state=tk.DISABLED, bg=DRK_GREY)
         self.update()
 
     def add_track(self, *args):
@@ -340,8 +376,9 @@ class OBMApp(tk.Tk):
         if ((self.cur_style in ['old', 'new', 'ezy']
                 and len(self.style_list) >= 10)
                 or (self.cur_style == 'theme' and len(self.style_list) >= 1)):
-                    popupmessage('Current style full.')
+                    popupinfo('Current style full.')
                     return
+        self.track_list.remove(track)
         if self.cur_style == 'old':
             self.obm_data.old_style.append(track)
         elif self.cur_style == 'new':
@@ -357,6 +394,7 @@ class OBMApp(tk.Tk):
             track = self.style_list[self.style_listbox.curselection()[0]]
         else:
             return
+        self.track_list.append(track)
         if self.cur_style == 'old':
             self.obm_data.old_style.remove(track)
         elif self.cur_style == 'new':
@@ -368,6 +406,8 @@ class OBMApp(tk.Tk):
         self.update()
 
     def clear_style(self):
+        for track in self.style_list:
+            self.track_list.append(track)
         if self.cur_style == 'old':
             self.obm_data.old_style.clear()
         elif self.cur_style == 'new':
@@ -386,6 +426,16 @@ class OBMApp(tk.Tk):
         self.style_listbox.delete(0, tk.END)
         for track in self.style_list:
             self.style_listbox.insert(tk.END, track)
+
+    def save_obm(self, directory=None):
+        if not directory:
+            directory = filedialog.askdirectory()
+        metadata = [self.meta_name.get(), self.meta_shortname.get(),
+                self.meta_version.get(), self.meta_description.get(),
+                self.meta_origin.get()]
+        self.obm_data.meta_data = dict(zip(self.obm_data.meta_data.keys(),
+                                           metadata))
+        self.obm_data.create_file(obm_dir=directory, music_dir=self.music_dir)
 
 
 if __name__ == '__main__':
